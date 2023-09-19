df764a5f0776fb4b62ea54fcdc6ce344e25fdb72
gokrusk <nigelljama@gmail.com>
2023-09-19 17:13:08 -0500

    types

--- Module 01 Exercise/module01.ts
+++ Module 01 Exercise/module01.ts
@@ -1,4 +1,47 @@
 function addNumbers(x: number, y: number) {
   return x + y;
 }
-console.log(addNumbers(3, 6));
+
+let x: number = 4;
+let y = 6;
+let flag: boolean;
+let yes = true;
+let no = false;
+let s: string = "Suma:";
+console.log(`${s} ${addNumbers(x, y)}`);
+
+// * list of values which contains the numbers 1,2 or 3
+enum ContractStatus {
+  Permanent,
+  Temp,
+  Apprentice,
+}
+
+let employee: ContractStatus = ContractStatus.Temp;
+console.log(employee); // ? 1
+
+// * any: no restrictions
+let randomValue: any = 10;
+randomValue = false;
+randomValue = "ok";
+console.log(randomValue.toUpperCase());
+
+let otherValue: unknown = 10;
+otherValue = "xd";
+console.log((<string>otherValue).toUpperCase());
+
+// * union types
+let multiType: number | boolean;
+multiType = 20; //* Valid
+multiType = true; //* Valid
+// multiType = "twenty";   //! Invalid
+
+function add(x: number | string, y: number | string) {
+  if (typeof x === "string" && typeof y === "string") return x.concat(y);
+  if (typeof x === "number" && typeof y === "number") return x + y;
+  throw new Error("Parameters must be numbers or string");
+}
+
+console.log(add(3, 6));
+console.log(add("2", "5"));
+console.log(add("one", 2)); //* Returns error
--- build/module01.js
+++ build/module01.js
@@ -2,4 +2,42 @@
 function addNumbers(x, y) {
     return x + y;
 }
-console.log(addNumbers(3, 6));
+let x = 4;
+let y = 6;
+let flag;
+let yes = true;
+let no = false;
+let s = "Suma:";
+console.log(`${s} ${addNumbers(x, y)}`);
+// * list of values which contains the numbers 1,2 or 3
+var ContractStatus;
+(function (ContractStatus) {
+    ContractStatus[ContractStatus["Permanent"] = 0] = "Permanent";
+    ContractStatus[ContractStatus["Temp"] = 1] = "Temp";
+    ContractStatus[ContractStatus["Apprentice"] = 2] = "Apprentice";
+})(ContractStatus || (ContractStatus = {}));
+let employee = ContractStatus.Temp;
+console.log(employee); // ? 1
+// * any: no restrictions
+let randomValue = 10;
+randomValue = false;
+randomValue = "ok";
+console.log(randomValue.toUpperCase());
+let otherValue = 10;
+otherValue = "xd";
+console.log(otherValue.toUpperCase());
+// * union types
+let multiType;
+multiType = 20; //* Valid
+multiType = true; //* Valid
+// multiType = "twenty";   //! Invalid
+function add(x, y) {
+    if (typeof x === "string" && typeof y === "string")
+        return x.concat(y);
+    if (typeof x === "number" && typeof y === "number")
+        return x + y;
+    throw new Error("Parameters must be numbers or string");
+}
+console.log(add(3, 6));
+console.log(add("2", "5"));
+console.log(add("one", 2)); //* Returns error
